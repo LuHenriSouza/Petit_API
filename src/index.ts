b4b591b0a6fd07b929da2760881d9451cf1c1606
Luiz <luizhapsouza@gmail.com>
2023-12-18 21:22:47 -0300

    fixing errors for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
+import { Knex } from './server/database/knex';
 import { server } from './server/server';
-server.listen(process.env.PORT || 3333, () => console.log('App rodando!'));
+
+const startServer = () => {
+    server.listen(process.env.PORT || 3333, () => {
+        console.log('App running!');
+    }
+    );
+};
+
+if (process.env.IS_LOCALHOST !== 'true') {
+    Knex.migrate.latest()
+        .then(() => {
+            startServer();
+        })
+        .catch(console.log);
+} else {
+    startServer();
+}
