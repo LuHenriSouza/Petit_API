2ae08174213bb9c6cf07afbaf5ecc71602225717
Luiz <luizhapsouza@gmail.com>
2023-12-20 11:40:10 -0300

    fix Count and DeleteById from Products and Fincash

--- src/server/database/providers/Fincash/Count.ts
+++ src/server/database/providers/Fincash/Count.ts
@@ -5,6 +5,7 @@ export const count = async (filter = ''): Promise<number | Error> => {
     try {
         const [{ count }] = await Knex(ETableNames.fincashs)
             .where('opener', 'like', `%${filter}%`)
+            .andWhere('deleted_at', null)
             .count<[{ count: number }]>('* as count');
         if (Number.isInteger(Number(count))) return Number(count);
 
--- src/server/database/providers/Fincash/DeleteById.ts
+++ src/server/database/providers/Fincash/DeleteById.ts
@@ -3,15 +3,20 @@ import { Knex } from '../../knex';
 
 export const deleteById = async (id: number): Promise<void | Error> => {
     try {
-        const result = await Knex(ETableNames.fincashs)
-            .update({
-                deleted_at: Knex.fn.now(),
-            })
-            .where('id', '=', id);
+        const deleted = await Knex(ETableNames.fincashs).select('id').where('id', id).andWhere('deleted_at', null).first();
+        if (deleted) {
+            const result = await Knex(ETableNames.fincashs)
+                .update({
+                    deleted_at: Knex.fn.now(),
+                })
+                .where('id', '=', id);
 
-        if (result > 0) return;
+            if (result > 0) return;
 
-        return new Error('Delete Failed');
+            return new Error('Delete Failed');
+        } else {
+            return new Error('This Object has already been deleted');
+        }
     } catch (e) {
         console.log(e);
         return new Error('Delete Failed');
