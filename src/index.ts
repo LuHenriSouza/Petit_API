cd6a3beaf376086ff8302d8a13cd085dc5082d53
Luiz <luizhapsouza@gmail.com>
2023-12-27 15:13:23 -0300

    added a console.log when start server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Knex } from './server/database/knex';
 import { server } from './server/server';
+import 'dotenv/config';
 
 const startServer = () => {
     server.listen(process.env.PORT || 3333, () => {
-        console.log('App running!');
+        console.log('App running! port: '+ (process.env.PORT || 3333) + 'CORS: ' + process.env.ENABLED_CORS);
     }
     );
 };
