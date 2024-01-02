12d0acbfb8b99234d87c7fc26aac152a9fdfd5ee
luizhapsouza <luizhapsouza@gmail.com>
2024-01-02 18:28:11 -0300

    feat: improved SaleDetail GetAllById

--- src/server/database/providers/SaleDetail/GetAllById.ts
+++ src/server/database/providers/SaleDetail/GetAllById.ts
@@ -1,8 +1,13 @@
 import { ETableNames } from '../../ETableNames';
 import { Knex } from '../../knex';
-import { ICashOutflow } from '../../models';
+import { ISaleDetails } from '../../models';
 
-export const getAllById = async (page: number, limit: number, sale_id: number): Promise<ICashOutflow[] | Error> => {
+interface IResponse {
+    data: ISaleDetails[],
+    totalValue: number,
+}
+
+export const getAllById = async (page: number, limit: number, sale_id: number): Promise<IResponse | Error> => {
     try {
         const sale = await Knex(ETableNames.sales).select('*').where('id', sale_id).first();
         if (sale) {
@@ -11,7 +16,20 @@ export const getAllById = async (page: number, limit: number, sale_id: number): 
                 .where('sale_id', sale_id).andWhere('deleted_at', null)
                 .offset((page - 1) * limit)
                 .limit(limit);
-            return result;
+
+            const countResult = await Knex(ETableNames.saleDetails)
+                .where('sale_id', sale_id)
+                .andWhere('deleted_at', null)
+                .sum('pricetotal as totalValue')
+                .first();
+
+            const totalValue = countResult ? countResult.totalValue || 0 : 0;
+
+            return {
+                data: result,
+                totalValue: totalValue,
+            };
+            
         } else {
             return Error('Sale not found');
         }
