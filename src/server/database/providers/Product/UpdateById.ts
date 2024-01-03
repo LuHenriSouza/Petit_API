6c673a7c4c5f598d6cf712b800fa87f9014fabe0
luizhapsouza <luizhapsouza@gmail.com>
2024-01-03 17:41:48 -0300

    Improved Product updateById

--- src/server/database/providers/Product/UpdateById.ts
+++ src/server/database/providers/Product/UpdateById.ts
@@ -4,18 +4,22 @@ import { IProduct } from '../../models';
 
 export const updateById = async (id: number, product: Omit<IProduct, 'id' | 'created_at' | 'updated_at' | 'code'>): Promise<void | Error> => {
     try {
-        const result = await Knex(ETableNames.products)
-            .update({
-                ...product,
-                updated_at: Knex.fn.now(),
-            })
-            .where('id', '=', id);
+        const deleted = await Knex(ETableNames.products).select('id', 'code').where('id', id).andWhere('deleted_at', null).first();
+        if (deleted) {
+            const result = await Knex(ETableNames.products)
+                .update({
+                    ...product,
+                    updated_at: Knex.fn.now(),
+                })
+                .where('id', '=', id);
 
-        if (result > 0) {
-            return;
+            if (result > 0) {
+                return;
+            }
+
+        } else {
+            return new Error('This Object has been deleted');
         }
-
-        return new Error('Update Failed');
     } catch (e) {
         console.log(e);
         return new Error('Update Failed');
