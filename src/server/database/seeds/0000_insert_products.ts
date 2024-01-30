275f823e1f891782118be9b8e15799bda45dbe47
luizhapsouza <luizhapsouza@gmail.com>
2024-01-30 17:32:13 -0300

    Improved seed Product

--- src/server/database/seeds/0000_insert_products.ts
+++ src/server/database/seeds/0000_insert_products.ts
@@ -17,19 +17,27 @@ for (let i = 0; i < NumberOfInsertions; i++) {
     productPrice.push(randNumber(3) / 100);
 }
 
+const generateRandomTimestamp = () => {
+    const now = new Date().getTime();
+    const randomOffset = Math.floor(Math.random() * 1000 * 60 * 60 * 24); // Random offset within 24 hours
+    return new Date(now + randomOffset);
+};
+
 export const seed = async () => {
     // const [{ count }] = await Knex(ETableNames.products).count<[{ count: number }]>('* as count');
 
     // if (!Number.isInteger(count) || count > 0) return;
 
-    const productsToInsert: Omit<IProduct, 'id' | 'created_at' | 'updated_at'>[] = [];
+    const productsToInsert: Omit<IProduct, 'id' | 'created_at'>[] = [];
 
     for (let i = 0; i < NumberOfInsertions; i++) {
+        const updatedAt = generateRandomTimestamp();
         productsToInsert.push({
             code: productCode[i],
             name: productName[i],
             sector: productSector[i],
             price: productPrice[i],
+            updated_at: updatedAt,
 
         });
     }
