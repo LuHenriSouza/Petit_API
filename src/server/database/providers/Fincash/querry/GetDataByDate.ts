d0802e12c87114549003fc75898de528cca42d31
luizhapsouza <luizhapsouza@gmail.com>
2024-07-25 16:07:37 -0300

    testing get data by date

---
+++ src/server/database/providers/Fincash/querry/GetDataByDate.ts
@@ -0,0 +1,26 @@
+import { Knex } from '../../../knex';
+import { ETableNames } from '../../../ETableNames';
+
+export interface IResponse {
+    id: number;
+    cardValue: number;
+    totalValue: number;
+    finalDate: Date;
+}
+
+export const getDataByDate = async (init: Date, end: Date): Promise<IResponse[] | Error> => {
+    try {
+        const result = await Knex<IResponse>(ETableNames.fincashs)
+            .select(
+                'id',
+                'cardValue',
+                'totalValue',
+                'finalDate'
+            )
+            .whereBetween('finalDate', [init, end]);
+
+        return result;
+    } catch (error) {
+        throw new Error(`Failed to fetch data: ${error}`);
+    }
+};
