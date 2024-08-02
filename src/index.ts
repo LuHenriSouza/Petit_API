e38a9c729138a093b2b2fc31126435a575d196ef
luizhapsouza <luizhapsouza@gmail.com>
2024-08-02 17:39:06 -0300

    feat: https added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import { server } from './server/server';
-import 'dotenv/config';
+import https from 'https';
+import fs from 'fs';
+import 'dotenv/config'; // Certifique-se de que o dotenv está carregando as variáveis de ambiente
+import { server } from './server/server'; // Certifique-se de que este export está correto
 
 const startServer = () => {
-    server.listen(process.env.PORT || 3333, () => {
-        console.log('App running! port: ' + (process.env.PORT || 3333) + 'CORS: ' + process.env.ENABLED_CORS);
-    }
-    );
+    // Carregar certificados
+    const options = {
+        key: fs.readFileSync('C:\\Users\\user\\Documents\\GitHub\\nginx-1.26.1\\ssl\\private.key'),
+        cert: fs.readFileSync('C:\\Users\\user\\Documents\\GitHub\\nginx-1.26.1\\ssl\\certificate.crt'),
+    };
+
+    // Configurar o servidor HTTPS
+    const httpsServer = https.createServer(options, server);
+
+    httpsServer.listen(process.env.PORT || 3333, () => {
+        console.log(`App running! Port: ${process.env.PORT || 3333} CORS: ${process.env.ENABLED_CORS}`);
+    });
 };
 
 startServer();
