28f8a268c32f4eb5447bcdadde3620937a7b8f6a
Luiz <luizhapsouza@gmail.com>
2024-09-14 16:12:09 -0300

    added a orderBy option in getAll Stock Function

--- src/server/controllers/Stock/GetAll.ts
+++ src/server/controllers/Stock/GetAll.ts
@@ -13,14 +13,14 @@ interface IQueryProps {
     page?: number,
     limit?: number,
     filter?: string,
-
+    orderBy?: string,
 }
 
 const queryValidation: yup.Schema<IQueryProps> = yup.object().shape({
     page: yup.number().moreThan(0),
     limit: yup.number().moreThan(0),
     filter: yup.string(),
-
+    orderBy: yup.string().oneOf(['updated_at', 'stock']),
 });
 
 export const getAllValidation = validation({
@@ -28,7 +28,7 @@ export const getAllValidation = validation({
 });
 
 export const getAll: RequestHandler = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
-    const result = await StockProvider.getAll(req.query.page || DEFAULT_PAGE, req.query.limit || DEFAULT_LIMIT, req.query.filter || DEFAULT_FILTER);
+    const result = await StockProvider.getAll(req.query.page || DEFAULT_PAGE, req.query.limit || DEFAULT_LIMIT, req.query.filter || DEFAULT_FILTER, req.query.orderBy);
     const count = await StockProvider.count(req.query.filter || DEFAULT_FILTER);
 
     if (result instanceof Error) {
