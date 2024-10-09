a056bbfbf07573d52fd98d4799be0b05ae7724a3
luizhapsouza <luizhapsouza@gmail.com>
2024-10-09 11:52:38 -0300

    feat: EditCashOutflow FincashUpdate midleware created

--- src/server/database/providers/CashOutflow/EditById.ts
+++ src/server/database/providers/CashOutflow/EditById.ts
@@ -1,6 +1,7 @@
 import { IBodyOutflow } from '../../../controllers/CashOutflow/types';
 import { ETableNames } from '../../ETableNames';
 import { Knex } from '../../knex';
+import { getBreak } from '../Fincash/CalcBreak';
 
 export const EditById = {
     update: async (outflow_id: number, outflow: IBodyOutflow) => {
@@ -18,6 +19,9 @@ export const EditById = {
                 .where('id', '=', outflow_id);
 
             if (result > 0) {
+                const fincash_id = outflowExist.fincash_id;
+                const breakUpdate = await updateFincash(fincash_id);
+                if (breakUpdate instanceof Error) return breakUpdate;
                 return;
             }
 
@@ -40,11 +44,10 @@ export const EditById = {
                 .insert(outflow)
                 .returning('id');
 
-            if (typeof result === 'object') {
-                return result.id;
-
-            } else if (typeof result === 'number') {
-                return result;
+            if (result) {
+                const breakUpdate = await updateFincash(fincash_id);
+                if (breakUpdate instanceof Error) return breakUpdate;
+                return;
             }
 
             return new Error('Register Failed');
@@ -53,31 +56,20 @@ export const EditById = {
             return new Error('Add Failed');
         }
     },
-    softDelete: async (outflow_id: number) => {
-        try {
-            const result = await Knex(ETableNames.cashOutflows)
-                .update({
-                    deleted_at: Knex.fn.now(),
-                })
-                .where('id', '=', outflow_id);
-
-            if (result > 0) {
-                return;
-            }
-
-            return new Error('Delete Failed');
-        } catch (e) {
-            console.log(e);
-            return new Error('Delete Failed');
-        }
-    },
     hardDelete: async (outflow_id: number) => {
         try {
+            const outflowExist = await Knex(ETableNames.cashOutflows)
+                .select('*')
+                .where('id', outflow_id)
+                .first();
+            if (!outflowExist) return new Error('Outflow not found');
             const result = await Knex(ETableNames.cashOutflows)
                 .where('id', '=', outflow_id)
                 .del();
 
             if (result > 0) {
+                const breakUpdate = await updateFincash(outflowExist.fincash_id);
+                if (breakUpdate instanceof Error) return breakUpdate;
                 return;
             }
 
@@ -87,4 +79,47 @@ export const EditById = {
             return new Error('Delete Failed');
         }
     },
+    // softDelete: async (outflow_id: number) => {
+    //     try {
+    //         const result = await Knex(ETableNames.cashOutflows)
+    //             .update({
+    //                 deleted_at: Knex.fn.now(),
+    //             })
+    //             .where('id', '=', outflow_id);
+
+    //         if (result > 0) {
+    //             return;
+    //         }
+
+    //         return new Error('Delete Failed');
+    //     } catch (e) {
+    //         console.log(e);
+    //         return new Error('Delete Failed');
+    //     }
+    // },
+};
+
+const updateFincash /* Break & Invoicing */ = async (fincash_id: number) => {
+    const fincash = await Knex(ETableNames.fincashs).select('*').where('id', fincash_id).first();
+    if (!fincash) return Error('Fincash not found');
+    if (fincash.deleted_at) return Error('This Fincash has been deleted');
+    if (!fincash.cardValue) return 0; //dont need to update
+
+    const breakCalc = await getBreak(fincash, fincash.cardValue);
+    if (breakCalc instanceof Error) return breakCalc;
+
+    const { realBreak, invoicing } = breakCalc;
+
+    const result = await Knex(ETableNames.fincashs)
+        .update({
+            invoicing,
+            break: realBreak,
+        })
+        .where('id', fincash_id);
+
+    if (result > 0)
+        return realBreak;
+    else
+        return Error('Internal Server Error');
+
 };
